e6487c13b4956264c5e87b08b3c78a18db88fce0
Alex Liu <mingzhangliu@gmail.com>
2025-05-29 20:44:23 +0800

    Changed background, text color and number config

--- poker-rng.py
+++ poker-rng.py
@@ -3,18 +3,20 @@ import random
 
 
 def update_number():
-    number = random.randint(1, 10)
+    number = random.randint(1, 100)
     number_label.config(text=str(number))
-    root.after(3000, update_number)
+    root.after(2000, update_number)
 
 
 root = tk.Tk()
 # root.overrideredirect(True) # removes title bar
 root.title("Poker RNG")
 root.geometry("150x100")
+root.configure(bg="black")
 
-number_label = tk.Label(root, text="", font=("Helvetica", 24))
+number_label = tk.Label(root, text="", font=("Helvetica", 24), fg="white", bg="black")
 number_label.pack(pady=20)
 
 update_number()
+
 root.mainloop()
