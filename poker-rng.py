794fa673c42b71c49330c7c86f69eb255efbdda5
Alex Liu <mingzhangliu@gmail.com>
2025-05-29 20:24:37 +0800

    Added number label

--- poker-rng.py
+++ poker-rng.py
@@ -3,6 +3,9 @@ import tkinter as tk
 root = tk.Tk()
 # root.overrideredirect(True) # removes title bar
 root.title("Poker RNG")
-root.geometry("200x100")
+root.geometry("150x100")
+
+number_label = tk.Label(root, text="1", font=("Helvetica", 24))
+number_label.pack(pady=20)
 
 root.mainloop()
