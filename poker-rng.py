4b047ec32caed9e17d4bd320011d909fb2e4eb63
Alex Liu <mingzhangliu@gmail.com>
2025-05-29 20:28:41 +0800

    Number label generates a random number every 3 seconds

--- poker-rng.py
+++ poker-rng.py
@@ -1,11 +1,20 @@
 import tkinter as tk
+import random
+
+
+def update_number():
+    number = random.randint(1, 10)
+    number_label.config(text=str(number))
+    root.after(3000, update_number)
+
 
 root = tk.Tk()
 # root.overrideredirect(True) # removes title bar
 root.title("Poker RNG")
 root.geometry("150x100")
 
-number_label = tk.Label(root, text="1", font=("Helvetica", 24))
+number_label = tk.Label(root, text="", font=("Helvetica", 24))
 number_label.pack(pady=20)
 
+update_number()
 root.mainloop()
